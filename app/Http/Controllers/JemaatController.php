923ab052c1df94918323f362b32211d7d5361a9e
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2025-12-01 12:09:33 +0700

    commit39

--- app/Http/Controllers/JemaatController.php
+++ app/Http/Controllers/JemaatController.php
@@ -14,11 +14,20 @@ class JemaatController extends Controller
      */
     public function index()
     {
-   
-        $events = TblEvent::whereMonth('tgl_event', Carbon::now()->month)
-            ->orderBy('tgl_event', 'asc')
-            ->paginate(10);
+        $latestEvent = TblEvent::orderBy('tgl_event', 'desc')->first();
 
+        if ($latestEvent) {
+            $latestMonth = Carbon::parse($latestEvent->tgl_event)->month;
+            $latestYear = Carbon::parse($latestEvent->tgl_event)->year;
+
+            $events = TblEvent::whereMonth('tgl_event', $latestMonth)
+                ->whereYear('tgl_event', $latestYear)
+                ->orderBy('tgl_event', 'asc')
+                ->paginate(10);
+        } else {
+            $events = collect(); // Tidak ada event sama sekali
+        }
+        
         return view('jemaat', compact('events'));
     }
 }
